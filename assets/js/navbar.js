8b95ef0b67553e70aa60502e8a7885e19e2f5451
Widinton Taimbud <wytaimbud77a@gmail.com>
2026-04-21 21:08:43 +0200

    feat/1-navbar

---
+++ assets/js/navbar.js
@@ -0,0 +1,30 @@
+export function loadNavbar() {
+  fetch("../components/navbar.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+
+      initNavbar();
+    })
+    .catch((err) => {
+      console.error("Error cargando navbar:", err);
+    });
+}
+
+/* FEATURE NAVBAR */
+function initNavbar() {
+  const hamburgerBtn = document.getElementById("navbar-hamburger");
+  const mobileMenu = document.getElementById("navbar-mobile-menu");
+
+  if (hamburgerBtn && mobileMenu) {
+    hamburgerBtn.addEventListener("click", () => {
+      mobileMenu.classList.toggle("open");
+    });
+
+    document.addEventListener("click", (e) => {
+      if (!hamburgerBtn.contains(e.target) && !mobileMenu.contains(e.target)) {
+        mobileMenu.classList.remove("open");
+      }
+    });
+  }
+}
